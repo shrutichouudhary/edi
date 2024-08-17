ab2ba01145d82ca2c47ea5f909da4e82d1a71d25
shruti <shruti12@s.amity.edu>
2024-08-17 19:18:36 +0530

    Add files via upload

---
+++ Editorial_site/scripts.js
@@ -0,0 +1,7 @@
+function downloadImage() {
+    window.location.href = 'https://code.visualstudio.com/Download';
+}
+
+function downloadPreviousVersions() {
+    window.location.href = 'https://github.com/git-for-windows/git/releases/download/v2.46.0.windows.1/Git-2.46.0-64-bit.exe';
+}
